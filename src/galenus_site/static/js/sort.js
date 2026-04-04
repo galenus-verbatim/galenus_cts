19b2345d1bd56c92ec66d658823cb9b5edd227bd
Charles Pletcher <cpletcher2@gmail.com>
2026-04-04 11:07:23 -0400

    Move static site to its own repository

--- src/galenus_site/static/js/sort.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('selnav').addEventListener('change', function(e) {
-  document.querySelectorAll('.title-list').forEach(function(el) { el.hidden = true; });
-  document.getElementById('list-' + e.target.value).hidden = false;
-});
